24dc582b76832516b7557955a982986750d26f33
piyushgarg22 <pgarg2020@gmail.com>
2019-12-01 13:14:45 +0530

    Updates

---
+++ precache-manifest.cd2067d9a1fa5ed8b9945a515efeb924.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bcb02a5f0cb59cc3f83b861a2666b52",
+    "url": "/campaign-manager/index.html"
+  },
+  {
+    "revision": "4deaf15d7579cafbb04e",
+    "url": "/campaign-manager/static/css/main.e37caed7.chunk.css"
+  },
+  {
+    "revision": "c45f37a0a9a90ede11ed",
+    "url": "/campaign-manager/static/js/2.267b7031.chunk.js"
+  },
+  {
+    "revision": "4deaf15d7579cafbb04e",
+    "url": "/campaign-manager/static/js/main.f6adfa96.chunk.js"
+  },
+  {
+    "revision": "6c3f42faf1329e15f5f8",
+    "url": "/campaign-manager/static/js/runtime-main.d0075fb5.js"
+  }
+]);
